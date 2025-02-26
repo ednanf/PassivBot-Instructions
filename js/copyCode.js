0fe8efa9322d9e6efe74981e063b82868c217a33
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2025-02-26 20:51:05 -0300

    Enhance clipboard functionality with event handling and feedback messages

--- js/copyCode.js
+++ js/copyCode.js
@@ -1,4 +1,14 @@
-function copyToClipboard() {
+function copyToClipboard(event) {
   var copyText = document.getElementById("CopyCode");
-  navigator.clipboard.writeText(copyText.innerText);
+  navigator.clipboard
+    .writeText(copyText.innerText)
+    .then(() => {
+      const button = event.target;
+      if (button.classList.contains("ru-text")) {
+        button.innerText = "✅ Скопировано!";
+      } else {
+        button.innerText = "✅ Copied!";
+      }
+    })
+    .catch((err) => console.error("Error copying text: ", err));
 }
